580ec4ca6f71293f649a364565cbb854126d2bac
esec-exploits <exploits@esec.sk>
2021-01-19 11:15:31 +0100

    update

--- CLIENT/Class/gui/ChatboxPanel.py
+++ CLIENT/Class/gui/ChatboxPanel.py
@@ -11,6 +11,7 @@
 # -------------------------------------
 
 import wx
+import wx.richtext as rt
 from Handler.button.SendMessageBtn import SendMessageBtn
 from Translation.English import English as Locale
 
@@ -20,11 +21,13 @@ class ChatboxPanel(wx.Panel):
 
         chatbox_sizer = wx.BoxSizer(wx.VERTICAL)
 
-        ChatboxPanel.chatbox = wx.TextCtrl(self, wx.ID_ANY, style=wx.TE_MULTILINE | wx.TE_READONLY)
+        ChatboxPanel.chatbox = rt.RichTextCtrl(self, wx.ID_ANY, style=wx.TE_MULTILINE | wx.TE_READONLY)
+        ChatboxPanel.chatbox.Bind(wx.EVT_SET_FOCUS, self.on_chatbox_focus)
 
         usermessage_hsizer = wx.BoxSizer(wx.HORIZONTAL)     
-        ChatboxPanel.messagebox = wx.TextCtrl(self, wx.ID_ANY, style=wx.TE_MULTILINE)
+        ChatboxPanel.messagebox = wx.TextCtrl(self, wx.ID_ANY, style=wx.TE_MULTILINE | wx.TE_PROCESS_ENTER)
         ChatboxPanel.messagebox.SetHint(Locale.send_message)
+        ChatboxPanel.messagebox.Bind(wx.EVT_TEXT_ENTER, SendMessageBtn)
         ChatboxPanel.sendbutton = wx.Button(self, wx.ID_ANY, label=Locale.send_button)
         ChatboxPanel.sendbutton.Bind(wx.EVT_BUTTON, SendMessageBtn)
         usermessage_hsizer.Add(ChatboxPanel.messagebox, 6, wx.EXPAND | wx.RIGHT, 10)
@@ -35,3 +38,9 @@ class ChatboxPanel(wx.Panel):
         chatbox_sizer.Add(usermessage_hsizer, 1, wx.ALL | wx.EXPAND, 10)
 
         self.SetSizer(chatbox_sizer)
+
+    def on_chatbox_focus(self, event):
+        try:
+            event.Veto()
+        except:
+            pass
--- CLIENT/Handler/button/SendMessageBtn.py
+++ CLIENT/Handler/button/SendMessageBtn.py
@@ -10,7 +10,10 @@
 #
 # -------------------------------------
 
+import wx
+import wx.richtext as rt
 from Translation.English import English as Locale
+import time
 
 class SendMessageBtn:
     def __init__(self, event):
@@ -20,7 +23,19 @@ class SendMessageBtn:
         current_message = ChatboxPanel.messagebox.GetValue()
 
         if current_message != "":
-            ChatboxPanel.chatbox.AppendText(current_message + "\n")
+            username_font = wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD, False, u"Ebrima")
+            message_font = wx.Font(10, wx.SWISS, wx.NORMAL, wx.NORMAL, False, u"Ebrima")
+
+
+            ChatboxPanel.chatbox.BeginParagraphSpacing(0, 0)
+            ChatboxPanel.chatbox.BeginFontSize(12)
+            ChatboxPanel.chatbox.WriteText("\n")
+            ChatboxPanel.chatbox.BeginBold()
+            ChatboxPanel.chatbox.WriteText("esec" + ": ")
+            ChatboxPanel.chatbox.EndBold()
+            ChatboxPanel.chatbox.WriteText(current_message)
+            ChatboxPanel.chatbox.ShowPosition(ChatboxPanel.chatbox.GetLastPosition())
+
             ChatboxPanel.messagebox.SetValue("")
             ChatboxPanel.messagebox.SetHint(Locale.send_message)
             ChatboxPanel.messagebox.SetFocus()
